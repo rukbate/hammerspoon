121672bf90dc4776d6e3e540c4ed40c62fc8800b
lin <rukbate@hotmail.com>
2019-06-02 15:30:28 +0800

    maximize window

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 require "weather"
 require "spoon"
+require "window"
 require "test"
-
-
-
 
---
+++ window.lua
@@ -0,0 +1,13 @@
+-- Maximize current window
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h
+    win:setFrame(f)
+ end)
