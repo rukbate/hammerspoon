faae5ea65638d994fd3b21b20de93fb831a0ad43
lin <rukbate@hotmail.com>
2019-06-07 10:35:49 +0800

    move window to left/right half

--- window.lua
+++ window.lua
@@ -11,3 +11,31 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", function()
     f.h = max.h
     win:setFrame(f)
  end)
+
+ -- Half the window to Left
+ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+ end)
+
+ -- Half the window to Right
+ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + max.w / 2
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+ end)
