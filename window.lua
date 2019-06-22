dc8fd06cda9c4309bd7e5d8c2c6b8c0a6a3eeb6f
lin <rukbate@hotmail.com>
2019-06-22 15:55:48 +0800

    window top, down and small middle

--- window.lua
+++ window.lua
@@ -27,6 +27,22 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
     win:setFrame(f)
 end)
 
+-- Move current window to middle of the screen, with 20% left in all sides
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+    local offsetX = max.w * 0.2
+    local offsetY = max.h * 0.2
+
+    f.x = max.x + offsetX
+    f.y = max.y + offsetY
+    f.w = max.w - offsetX * 2
+    f.h = max.h - offsetY * 2
+    win:setFrame(f)
+end)
+
  -- Half the window to Left
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
     local win = hs.window.focusedWindow()
@@ -52,5 +68,33 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
     f.y = max.y
     f.w = max.w / 2
     f.h = max.h
+    win:setFrame(f)
+end)
+
+-- Half the window to Top
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h / 2
+    win:setFrame(f)
+end)
+
+-- Half the window to bottom
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y + max.h / 2
+    f.w = max.w
+    f.h = max.h / 2
     win:setFrame(f)
 end)
